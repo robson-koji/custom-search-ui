3143ced394e35d016033bf9aabd59dacc3aa8dd4
Projeto Biblioteca Virtual FAPESP <rmoriya@fapesp.br>
2021-09-20 11:55:55 -0300

    eslint fix errors

--- packages/react-search-ui-views/src/MultiCheckboxFacet.js
+++ packages/react-search-ui-views/src/MultiCheckboxFacet.js
@@ -2,10 +2,18 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import { FacetValue } from "./types";
-import { appendClassName, getFilterValueDisplay, getFilterCleanValueDisplay } from "./view-helpers";
+import {
+  appendClassName,
+  getFilterValueDisplay,
+  getFilterCleanValueDisplay
+} from "./view-helpers";
+
+const hashCode = s =>
+  s.split("").reduce((a, b) => ((a << 5) - a + b.charCodeAt(0)) | 0, 0);
 
 function MultiCheckboxFacet({
-  type="parent",
+  child_id = null,
+  type = "parent",
   className,
   label,
   onMoreClick,
@@ -16,32 +24,56 @@ function MultiCheckboxFacet({
   showSearch,
   onSearch,
   searchPlaceholder
-})
-
-  {
-    const renderRec = (option) =>{ 
-      // debugger;
-      // console.log(option.value)      
-      if (option.children.length > 0){
-          return <MultiCheckboxFacet 
-          key={option.value} 
-          options={option.children} 
-          // label={label} 
-          type="child" 
+}) {
+  const renderRec = (option, id) => {
+    // debugger;
+    // console.log(option.value)
+    if (option.children.length > 0) {
+      return (
+        <MultiCheckboxFacet
+          child_id={id}
+          key={option.value}
+          options={option.children}
+          // label={label}
+          type="child"
           onSelect={onSelect}
           onRemove={onRemove}
           onSearch={onSearch}
           onMoreClick={onMoreClick}
           // showMore={showMore}
           // showSearch={showSearch}
+        />
+      );
+    }
+  };
 
-          />
+  const handleClick = (e, option, checked) => {
+    // Plus sign besides checkbox with children, call function to show children.
+    if (e.target.className === "sui-multi-checkbox-facet__has-children") {
+      const child_id = e.target.getAttribute("child");
+      const child = document.getElementById(child_id);
+      if (child.style.display === "none" || child.style.display === "") {
+        child.style.display = "block";
+        e.target.innerHTML = " − ";
+      } else {
+        child.style.display = "none";
+        e.target.innerHTML = " + ";
       }
-    } 
-
-
+      e.preventDefault();
+    } else if (e.defaultPrevented) {
+      return; // Exits here if event has been handled
+    } else {
+      // Block hierarchical facets checkbox event capture (bubbling)
+      e.preventDefault();
+      // Checkbox action
+      checked ? onRemove(option.value) : onSelect(option.value);
+    }
+  };
   return (
-    <fieldset className={appendClassName(`sui-facet-${type}`, className)}>
+    <fieldset
+      className={appendClassName(`sui-facet-${type}`, className)}
+      id={child_id}
+    >
       <legend className={`sui-facet-${type}__title`}>{label}</legend>
 
       {showSearch && (
@@ -60,8 +92,9 @@ function MultiCheckboxFacet({
       <div className="sui-multi-checkbox-facet">
         {options.length < 1 && <div>No matching options</div>}
         {options.map(option => {
+          const id_hash = hashCode(option.value);
           const checked = option.selected;
-          const has_children = Boolean(option.children.length > 0)
+          const has_children = Boolean(option.children.length > 0);
 
           return (
             <label
@@ -71,17 +104,19 @@ function MultiCheckboxFacet({
               )}`}
               className={`sui-multi-checkbox-facet__option-label-${type}`}
             >
-              <div className="sui-multi-checkbox-facet__option-input-wrapper">
+              <div
+                className="sui-multi-checkbox-facet__option-input-wrapper"
+                onClick={e => handleClick(e, option, checked)}
+              >
                 <div className="sui-multi-checkbox-facet__option-input-wrapper-left">
-                { 
-                has_children && 
-                <span className="sui-multi-checkbox-facet__has-children">
-                  +
-                </span>
-                }                  
-                 
-
-
+                  {has_children && (
+                    <span
+                      className="sui-multi-checkbox-facet__has-children"
+                      child={id_hash}
+                    >
+                      +
+                    </span>
+                  )}
                   <input
                     id={`example_facet_${label}${getFilterValueDisplay(
                       option.value
@@ -89,11 +124,7 @@ function MultiCheckboxFacet({
                     type="checkbox"
                     className="sui-multi-checkbox-facet__checkbox"
                     checked={checked}
-                    onChange={(_ev) =>{
-                      // debugger;
-                      checked ? onRemove(option.value) : onSelect(option.value)
-                    }
-                    }
+                    // onClick={e => handleClick(e, option, checked)}
                   />
                   <span className={`sui-multi-checkbox-facet__input-text`}>
                     {getFilterCleanValueDisplay(option.value)}
@@ -104,12 +135,7 @@ function MultiCheckboxFacet({
                 </span>
               </div>
 
-
-
-
-              { option.hasOwnProperty('children') && renderRec(option) }
-
-
+              {option.hasOwnProperty("children") && renderRec(option, id_hash)}
             </label>
           );
         })}
@@ -130,16 +156,18 @@ function MultiCheckboxFacet({
 }
 
 MultiCheckboxFacet.propTypes = {
-  // label: PropTypes.string.isRequired,
-  // onMoreClick: PropTypes.func.isRequired,
-  // onRemove: PropTypes.func.isRequired,
-  // onSelect: PropTypes.func.isRequired,
-  // onSearch: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onMoreClick: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(FacetValue).isRequired,
-  // showMore: PropTypes.bool.isRequired,
+  showMore: PropTypes.bool.isRequired,
   className: PropTypes.string,
   showSearch: PropTypes.bool,
-  searchPlaceholder: PropTypes.string
+  searchPlaceholder: PropTypes.string,
+  child_id: PropTypes.string.isRequired
 };
 
 export default MultiCheckboxFacet;
